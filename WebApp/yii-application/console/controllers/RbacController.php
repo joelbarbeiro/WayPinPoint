d01a2caf022df52516abd6ce33e0ebf7a4bea80d
aebarroso <andre.e.barroso@gmail.com>
2024-10-28 16:19:39 +0000

    #10-UserRules-Config files for user rules and basic set up

---
+++ WebApp/yii-application/console/controllers/RbacController.php
@@ -0,0 +1,41 @@
+<?php
+namespace console\controllers; //namespace for advanced Project
+use Yii;
+use yii\console\Controller;
+
+class RbacController extends Controller
+{
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
+        $auth->removeAll();
+
+        // add "createPost" permission
+        $createActivity = $auth->createPermission('createActivity');
+        $createActivity->description = 'Create a activity';
+        $auth->add($createActivity);
+
+        // add "updateActivity" permission
+        $updateActivity = $auth->createPermission('updateActivity');
+        $updateActivity->description = 'Update post';
+        $auth->add($updateActivity);
+
+        // add "client" role and give this role the "createPost" permission
+        $client = $auth->createRole('client');
+        $auth->add($client);
+        $auth->addChild($client, $createActivity);
+
+        // add "admin" role and give this role the "updateActivity" permission
+        // as well as the permissions of the "author" role
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $updateActivity);
+        $auth->addChild($admin, $client);
+
+        // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
+        // usually implemented in your User model.
+        $auth->assign($client, 2);
+        $auth->assign($admin, 1);
+        // php yii rbac/init no fim disto para fazer assing disto tudo
+    }
+}
