cc65ff1946053c21d66f3169416144d631b92c39
aebarroso <andre.e.barroso@gmail.com>
2024-10-28 17:11:57 +0000

    #52-added html template

--- WebApp/yii-application/frontend/views/layouts/main.php
+++ WebApp/yii-application/frontend/views/layouts/main.php
@@ -5,12 +5,15 @@
 
 use common\widgets\Alert;
 use frontend\assets\AppAsset;
+use frontend\assets\TemplateAsset;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
+TemplateAsset::register($this);
 AppAsset::register($this);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
