84bad0e711f533ceaadec52ae6f784fae326df3a
joelbarbeiro <145718412+joelbarbeiro@users.noreply.github.com>
2024-11-22 04:50:38 +0000

    #56 - CRUD Reviews - Reviews are now only showed for the activity they belong - Added empty screen Messages ( Could be changed ) - Added more permissions to RbacController.php - Added some css in site.css to card-img-container so the images are the same size

--- WebApp/yii-application/frontend/views/review/index.php
+++ WebApp/yii-application/frontend/views/review/index.php
@@ -9,27 +9,40 @@ use yii\helpers\Url;
 
 $this->title = 'Review';
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerCssFile('@web/css/site.css');
+
 ?>
 <div class="reviews-index">
-
     <h1><?= Html::encode($this->title) ?></h1>
     <p>
         <?= Html::a('Create Review', ['create', 'id' => $activityId], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php
-    foreach ($dataProvider->models as $review) {
-        $imgPath = Url::to('@web/assets/uploads/' . $review->activity->user_id . '/');
-        echo '<div class="card m-5" >';
-
-        echo '<img src="' . $imgPath . $review->activity->photo . '" class="card-img-top" alt="...">';
-        echo '<div class="card-body">';
-        echo '<h5 class="card-title">' . 'Activity : ' . $review->activity->name . '</h5>';
-        echo '<p class="card-text">' . 'Score : ' . $review->score . ' out of 5.</p>';
-        echo '<p class="card-text">' . 'Comment : ' . $review->message . '</p>';
-        echo '<p class="card-text">' . 'Creation Date : ' . $review->created_at . '</p>';
-        echo '</div>';
-    }
-    ?>
-
+    <div class="container">
+        <div class="row justify-content-center g-4">
+            <?php
+            if (empty($activitiesReview)) {
+                echo '<div class="d-flex flex-column justify-content-center align-items-center vh-50">';
+                echo '<h1 class="text-center mb-4">No Reviews for this Activity</h1>';
+                echo '<i class="fa fa-sad-tear fa-5x text-muted"></i>';
+                echo '</div>';
+            } else {
+                foreach ($activitiesReview as $review) {
+                    $imgPath = Url::to('@web/assets/uploads/' . $review->activity->user_id . '/');
+                    echo '<div class="col-md-12 col-sm-12 d-flex align-items-stretch mb-12 px-3">';
+                    echo '<div class="card w-100 ">';
+                    echo '<img src="' . $imgPath . $review->activity->photo . '" class="card-img-top card-img-container" alt="Activity Image">';
+                    echo '<div class="card-body">';
+                    echo '<h5 class="card-title">' . 'Activity: ' . Html::encode($review->activity->name) . '</h5>';
+                    echo '<p class="card-text">' . 'Score: ' . Html::encode($review->score) . ' out of 5.</p>';
+                    echo '<p class="card-text">' . 'Comment: ' . Html::encode($review->message) . '</p>';
+                    echo '<p class="card-text">' . 'Creation Date: ' . Html::encode($review->created_at) . '</p>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            }
+            ?>
+        </div>
+    </div>
 </div>
