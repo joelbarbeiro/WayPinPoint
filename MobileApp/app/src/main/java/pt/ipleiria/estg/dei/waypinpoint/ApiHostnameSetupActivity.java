739998cbd50f7ef62d6eb54b72a1acf1d9410db2
aebarroso <andre.e.barroso@gmail.com>
2024-12-15 17:31:19 +0000

    - View Config Hostname - Need to test

---
+++ MobileApp/app/src/main/java/pt/ipleiria/estg/dei/waypinpoint/ApiHostnameSetupActivity.java
@@ -0,0 +1,46 @@
+package pt.ipleiria.estg.dei.waypinpoint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ApiHostnameSetupActivity extends AppCompatActivity {
+
+    private EditText etHostname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_hostname_setup);
+
+        etHostname = findViewById(R.id.textEditHostname);
+        SharedPreferences sharedPreferences = getSharedPreferences("API_HOSTNAME", Context.MODE_PRIVATE);
+
+        if(sharedPreferences.contains("API_HOSTNAME")) {
+            etHostname.setText(sharedPreferences.getString("API_HOSTNAME", "35.179.107.54"));
+        }
+
+    }
+    public void onClickSaveHostname(View view){
+        String hostname;
+        hostname = etHostname.getText().toString();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("API_HOSTNAME", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("API_HOSTNAME", hostname);
+        editor.apply();
+
+    }
+
+
+    public void onClickHostnameBack(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
