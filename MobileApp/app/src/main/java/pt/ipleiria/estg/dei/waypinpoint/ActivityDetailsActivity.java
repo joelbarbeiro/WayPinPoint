fb0270282c247f2b10f14dcf71dc4dcfac10a946
pedrolourenco <2232494@my.ipleiria.pt>
2024-12-26 19:04:38 +0000

    #69 - Crud Cart - More fixes

--- MobileApp/app/src/main/java/pt/ipleiria/estg/dei/waypinpoint/ActivityDetailsActivity.java
+++ MobileApp/app/src/main/java/pt/ipleiria/estg/dei/waypinpoint/ActivityDetailsActivity.java
@@ -77,6 +77,7 @@ public class ActivityDetailsActivity extends AppCompatActivity {
 
 
         btBuyActivity = findViewById(R.id.btBuyActivity);
+        loadActivity();
         btBuyActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
