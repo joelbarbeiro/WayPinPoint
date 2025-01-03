9c70f59078482421d105a55b856e93ea5fd0461b
pedrolourenco <2232494@my.ipleiria.pt>
2025-01-03 16:37:04 +0000

    #69 - Commented Basic Auth

--- WebApp/yii-application/backend/modules/api/controllers/CartController.php
+++ WebApp/yii-application/backend/modules/api/controllers/CartController.php
@@ -23,26 +23,26 @@ class CartController extends ActiveController
 
     public $modelClass = 'common\models\Cart';
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-            'class' => HttpBasicAuth::className(), // ou QueryParamAuth::className(),
-            'except' => ['index', 'view'],
-            'auth' => [$this, 'authintercept']
-        ];
-        return $behaviors;
-    }
-
-    public function authintercept($username, $password)
-    {
-        $user = User::findByUsername($username);
-        if ($user && $user->validatePassword($password)) {
-            $this->user = $user; //Guardar user autenticado         r
-            return $user;
-        }
-        throw new \yii\web\ForbiddenHttpException('Error auth'); //403
-    }
+//    public function behaviors()
+//    {
+//        $behaviors = parent::behaviors();
+//        $behaviors['authenticator'] = [
+//            'class' => HttpBasicAuth::className(), // ou QueryParamAuth::className(),
+//            'except' => ['index', 'view'],
+//            'auth' => [$this, 'authintercept']
+//        ];
+//        return $behaviors;
+//    }
+//
+//    public function authintercept($username, $password)
+//    {
+//        $user = User::findByUsername($username);
+//        if ($user && $user->validatePassword($password)) {
+//            $this->user = $user; //Guardar user autenticado         r
+//            return $user;
+//        }
+//        throw new \yii\web\ForbiddenHttpException('Error auth'); //403
+//    }
 
     public function actionCount()
     {
