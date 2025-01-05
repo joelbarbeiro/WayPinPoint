2790ee2b8518baa4a4b62031946ff05e0b260513
pedrolourenco <2232494@my.ipleiria.pt>
2025-01-05 18:29:43 +0000

    #69 Cart Unit Test Finished

--- WebApp/yii-application/common/tests/unit/CartTest.php
+++ WebApp/yii-application/common/tests/unit/CartTest.php
@@ -13,12 +13,21 @@ class CartTest extends \Codeception\Test\Unit
     /**
      * @var \common\tests\UnitTester
      */
+    private const VALID_NAME = 'Test activity';
+    private const VALID_DESCRIPTION = 'Test activity description';
+    private const VALID_INT = '10';
+    private const VALID_ADDRESS = 'Test activity address nÂº 4 3 esq, 1500-000, Lisboa';
+    private const VALID_PHOTO = 'c:\testFile.jpg';
+    private const VALID_DATE = ['0' => '2025-01-01', '1' => '2025-02-01'];
+    private const VALID_HOUR = ['0' => '10', '1' => '20'];
+    private const INVALID_DATE = '2025-01-01';
+    private const INVALID_HOUR = '12:00';
     private const STRING_80_CHARS = 'jofxabzawssmlxopymkbkglwunpidwopvpsiabrcrdctieetcahewubomqcseojxbhbzuijixsyofznn';
     private const INVALID_PRODUCT_ID = 0;
     private const INVALID_USER_ID = 0;
-    private const INVALID_QUANTITY = -1;
+    private const INVALID_QUANTITY = -1; //Needs to be minimum 1
     private const INVALID_CART_ID = 0;
-    private const INVALID_STATUS = 3;
+    private const INVALID_STATUS = 3; //Only allowed to be between 0 and 1
     private const INVALID_CALENDAR_ID = 0;
     private const VALID_PRODUCT_ID = 1;
     private const VALID_USER_ID = 1;
@@ -26,14 +35,23 @@ class CartTest extends \Codeception\Test\Unit
     private const VALID_STATUS = 0;
     private const VALID_CALENDAR_ID = 1;
     protected UnitTester $tester;
+    protected $user;
+    protected $category;
+    protected $activity;
 
     protected function _before()
     {
-        $user = $this->createValidUser();
-        $user->save();
+        $this->user = $this->createValidUser();
+        $this->user->save();
 
-        $activity = $this->createValidActivity($user);
-        $activity->save();
+
+        $this->category = $this->createCategory();
+        $this->category->save();
+
+
+        $this->activity = $this->createValidActivity();
+        $this->activity->save();
+
 
         $cart = $this->createValidCart();
         $cart->save();
@@ -86,8 +104,7 @@ class CartTest extends \Codeception\Test\Unit
         $cartFromDatabase = Cart::find()->where(['product_id' => self::VALID_PRODUCT_ID])->one();
         $this->assertNotNull($cartFromDatabase);
         $this->assertEquals(self::VALID_PRODUCT_ID, $cartFromDatabase->product_id);
-        $this->assertEquals(self::VALID_USER_ID, $cartFromDatabase->user_id);
-        $this->assertEquals(self::VALID_QUANTITY, $cartFromDatabase->quantity);
+        $this->assertEquals( self::VALID_QUANTITY, $cartFromDatabase->quantity);
         $this->assertEquals(self::VALID_STATUS, $cartFromDatabase->status);
     }
 
@@ -95,7 +112,7 @@ class CartTest extends \Codeception\Test\Unit
     {
         $cart = new Cart();
         $cart->product_id = self::VALID_PRODUCT_ID;
-        $cart->user_id = self::VALID_USER_ID;
+        $cart->user_id = $this->user->id;
         $cart->quantity = self::VALID_QUANTITY;
         $cart->status = self::VALID_STATUS;
         $cart->calendar_id = self::VALID_CALENDAR_ID;
@@ -105,30 +122,33 @@ class CartTest extends \Codeception\Test\Unit
     private function createValidUser()
     {
         $user = new User();
-        $user->id = 1;
         $user->username = "pedro";
         $user->email = "pedro@gmail.com";
         $user->password = "pedro123123";
+
         return $user;
     }
-
-    private function createValidActivity($user)
-    {
+    public function createCategory(){
         $category = new Category();
-        $category->description = "Water Sports";
-        $category->save();
+        $category->description = "Test category";
+
+        return $category;
+    }
+    private function createValidActivity()
+    {
         $activity = new Activity();
-        $activity->name = "Surf";
-        $activity->address = "Rua da ponta";
-        $activity->photo = "c:/file.jpg";
-        $activity->user_id = $user->id;
-        $activity->maxpax = 3;
-        $activity->status = 1;
-        $activity->description = "Standard description";
-        $activity->priceperpax = 10;
-        $activity->category_id = $category->id;
-        $activity->date = ["0=>2025-02-03"];
-        $activity->hour = ["0=>23"];
+        $activity->name = self::VALID_NAME;
+        $activity->description = self::VALID_DESCRIPTION;
+        $activity->photo = self::VALID_PHOTO;
+        $activity->photoFile = self::VALID_PHOTO;
+        $activity->maxpax = self::VALID_INT;
+        $activity->priceperpax = self::VALID_INT;
+        $activity->address = self::VALID_ADDRESS;
+        $activity->category_id = $this->category->id;
+        $activity->date = self::VALID_DATE;
+        $activity->hour = self::VALID_HOUR;
+        $activity->user_id = $this->user->id;
+
         return $activity;
     }
 
@@ -168,6 +188,25 @@ class CartTest extends \Codeception\Test\Unit
         $this->assertTrue($wasSavedSuccessfully, "Cart should not be saved due to invalid status");
     }
 
+    public function createInvalidCart()
+    {
+        $cart = new Cart();
+        $cart->product_id = self::INVALID_PRODUCT_ID;
+        $cart->user_id = self::INVALID_USER_ID;
+        $cart->quantity = self::INVALID_QUANTITY;
+        $cart->calendar_id = self::INVALID_CALENDAR_ID;
+        return $cart;
+    }
+    public function testInvalidCart()
+    {
+        $cart = $this->createInvalidCart();
+        $this->assertFalse($cart->validate(['product_id']), 'Validation should fail for product_id');
+        $this->assertFalse($cart->validate(['user_id']), 'Validation should fail for user_id');
+        $this->assertFalse($cart->validate(['quantity']), 'Validation should fail for quantity');
+        $this->assertFalse($cart->validate(['calendar_id']), 'Validation should fail for calendar_id');
 
+        $wasSavedSuccessfully = $cart->save();
+        $this->assertFalse($wasSavedSuccessfully, "Cart should not be saved due to invalid status");
 
+    }
 }
